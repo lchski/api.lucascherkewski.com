933ec4c53cf5ada5d90c26a5d2e9645f0be022e9
Lucas Cherkewski <lucas@ecustom.ca>
2015-12-25 23:35:07 -0500

    Create migration route?

--- app/migrations/CreateThingsTableMigration.php
+++ app/migrations/CreateThingsTableMigration.php
@@ -9,7 +9,14 @@ class CreateThingsTableMigration extends Migration {
 	public function __construct() {
 		parent::__construct();
 
-		
+		$this->app->get('/migration/create_things_table', function($request, $response, $args) {
+			Capsule::schema()->create('things', function($table) {
+				$table->increments('id');
+				$table->string('title');
+			});
+
+			echo "Done.";
+		});
 	}
 
 }
