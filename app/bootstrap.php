27fdffe9b7bcbc97ba9361b1ea5b8faa03f71d6c
Lucas Cherkewski <lucas@ecustom.ca>
2015-12-25 22:04:49 -0500

    Add helpers

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,6 +3,9 @@
 // Load our Composer components.
 require __DIR__ . '/../vendor/autoload.php';
 
+// Load our helpers.
+require __DIR__ . '/helpers.php';
+
 // Load our Dotenv variables.
 $dotenv = new \Dotenv\Dotenv(__DIR__ . '/../');
 $dotenv->load();
