10afad8b039e7a851fddcd3a4a2e7a17594af7f4
Lucas Cherkewski <lucas@ecustom.ca>
2015-12-26 16:57:50 -0500

    A simple /things route

--- public/index.php
+++ public/index.php
@@ -5,8 +5,19 @@ define('APP_PATH', __DIR__ . '/../app/');
 
 require APP_PATH . 'bootstrap.php';
 
+// Our homepage route.
 $app->any('/', '\\Lchski\\MainController');
 
-$app->any('/things/{id}', '\\Lchski\\ThingController');
+// All our API routes, in one place.
+$app->group('/api', function() {
+	$this->get('/things', function( \Slim\Http\Request $request, \Slim\Http\Response $response, array $args ) {
+		$response
+			->write(
+				json_encode( \Lchski\Thing::all() )
+			);
+	});
+
+	$this->any('/things/{id}', '\\Lchski\\ThingController');
+});
 
 $app->run();
