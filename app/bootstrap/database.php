134312dd1f40f4c0fc54ece28478f3731fff2a4e
Lucas Cherkewski <lucas@ecustom.ca>
2015-12-26 23:29:11 -0500

    Now configure PhpOrient and change env vars

--- app/bootstrap/database.php
+++ app/bootstrap/database.php
@@ -1,23 +1,11 @@
 <?php
 
-use \Illuminate\Database\Capsule\Manager as Capsule;
+use PhpOrient\PhpOrient;
 
-$capsule = new Capsule;
-
-// Set up our default DB connection.
-$capsule->addConnection([
-	'driver'    => env('DB_DRIVER', 'mysql'),
-    'host'      => env('DB_HOST', 'localhost'),
-    'database'  => env('DB_NAME', 'database'),
-    'username'  => env('DB_USERNAME', 'root'),
-    'password'  => env('DB_PASSWORD', 'password'),
-    'charset'   => env('DB_CHARSET', 'utf8'),
-    'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
-    'prefix'    => env('DB_PREFIX', ''),
-]);
-
-// Allow our Capsule instance to be accessed via static methods.
-$capsule->setAsGlobal();
-
-// Set up the Eloquent ORM.
-$capsule->bootEloquent();
+$phporient= new PhpOrient();
+$phporient->configure( array(
+	'hostname' => env('DB_HOST', 'localhost'),
+	'username' => env('DB_USERNAME', 'root'),
+	'password' => env('DB_PASSWORD', 'root'),
+	'port'     => env('DB_PORT', 2424),
+) );
