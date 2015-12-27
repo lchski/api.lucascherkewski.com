69540829149ec6030657daa66ad67924d13ae754
Lucas Cherkewski <lucas@ecustom.ca>
2015-12-27 13:18:12 -0500

    Improve CreateDatabaseMigration

--- app/migrations/CreateDatabaseMigration.php
+++ app/migrations/CreateDatabaseMigration.php
@@ -7,10 +7,36 @@ use Lchski\Contracts\Migration;
 class CreateDatabaseMigration extends BaseMigration implements Migration
 {
 	/**
-	 * Using the OrientDB client from our DIC, create the database specified by the ENV variables.
+	 * The database on which we're performing our migration.
+	 *
+	 * @var mixed
+	 */
+	protected $database;
+
+	/**
+	 * CreateDatabaseMigration constructor.
+	 * @param PhpOrient $phporient
+	 */
+	public function __construct(PhpOrient $phporient)
+	{
+		parent::__construct($phporient);
+
+		$this->database = env('DB_NAME', 'lucascherkewskicom');
+	}
+
+	/**
+	 * Using the PhpOrient instance from our DIC, create the database specified by the ENV variables.
 	 */
 	public function up()
 	{
-		$this->phporient->dbCreate(env('DB_NAME', 'lucascherkewskicom'));
+		$this->phporient->dbCreate($this->database);
+	}
+
+	/**
+	 * Using the PhpOrient instance from our DIC, drop the database.
+	 */
+	public function down()
+	{
+		$this->phporient->dbDrop($this->database);
 	}
 }
