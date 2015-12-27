1c01a54dd66576ebbfd85283cf8d0a9159c6888d
Lucas Cherkewski <lucas@ecustom.ca>
2015-12-27 13:14:36 -0500

    Start to our OrientDb client

--- app/helpers/OrientDbHelper.php
+++ app/helpers/OrientDbHelper.php
@@ -3,7 +3,19 @@
 namespace Lchski\Helpers;
 
 
+use Slim\Container;
+
 class OrientDbHelper
 {
+	protected $phporient;
 
+	public function __invoke(Container $container)
+	{
+		// Check if our PhpOrient instance has been properly configured.
+		if (!$container->has('phporient')) {
+			throw new \RuntimeException("DI container does not provide `phporient`");
+		}
+
+		$this->phporient = $container->get('phporient');
+	}
 }
