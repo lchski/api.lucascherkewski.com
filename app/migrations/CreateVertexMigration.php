cb04e01ab4f4328064029ee3a0b97cbf2e657321
Lucas Cherkewski <lucas@ecustom.ca>
2015-12-27 21:03:11 -0500

    Add CreateVertex migration

---
+++ app/migrations/CreateVertexMigration.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Lchski\Migrations;
+
+
+use Lchski\Contracts\Migration;
+
+class CreateVertexMigration extends BaseMigration implements Migration
+{
+	/**
+	 * Create a dummy node.
+	 */
+	public function up()
+	{
+		$this->dbHelper->createNode(array('name' => 'DummyVertex'));
+	}
+
+	/**
+	 * Delete the node.
+	 */
+	public function down()
+	{
+
+	}
+}
