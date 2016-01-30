669857927e65abfe9afeab41af6da09c133cc1b4
Lucas Cherkewski <lucas@ecustom.ca>
2016-01-30 17:04:27 -0500

    Create migration to test Edge creation

---
+++ app/migrations/CreateConnectionMigration.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Lchski\Migrations;
+
+
+use Lchski\Contracts\Migration;
+
+class CreateConnectionMigration extends BaseMigration implements Migration
+{
+	/**
+	 * Create a dummy connection.
+	 */
+	public function up()
+	{
+		$this->dbHelper->createConnection(array('from' => "(select from V where name = 'DummyVertex')", 'to' => "(select from V where name = 'DummyVertex')"), array('description' => 'Yoyo!'));
+	}
+
+	/**
+	 * Delete the connection.
+	 */
+	public function down()
+	{
+		$this->dbHelper->deleteNodes(array('from' => "(select from V where name = 'DummyVertex')", 'to' => "(select from V where name = 'DummyVertex')"));
+	}
+}
