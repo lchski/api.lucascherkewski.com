08c4fdb7fcc774f6a00684e282b5feffb16df18c
Lucas Cherkewski <lucas@ecustom.ca>
2016-01-30 22:41:21 -0500

    simplify index.php front controller

--- public/index.php
+++ public/index.php
@@ -1,6 +1,4 @@
 <?php
 
-// The path to our application folder.
-define('APP_PATH', __DIR__ . '/../app/');
-
-require APP_PATH . 'bootstrap.php';
+// Load up our bootstrap from the application folder.
+require __DIR__ . '/../app/' . 'bootstrap.php';
