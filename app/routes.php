6d0f3812f84068db408946ba0943e9695e74ea75
Lucas Cherkewski <lucas@ecustom.ca>
2016-01-30 22:07:35 -0500

    woo, we can create new things now!

--- app/routes.php
+++ app/routes.php
@@ -19,5 +19,15 @@ $app->group('/api', function() {
 			);
 	});
 
+	$this->post('/things', function( \Slim\Http\Request $request, \Slim\Http\Response $response, array $args ) {
+		$dbHelper = $this->get('orientdb_helper');
+
+		return $this->response
+			->withHeader('Content-type', 'application/json')
+			->write(
+				json_encode( $dbHelper->createNode($request->getParsedBody()) )
+			);
+	});
+
 	$this->any('/things/{id}', '\\Lchski\\ThingController');
 });
