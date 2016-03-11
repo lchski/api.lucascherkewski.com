172f50061e9e2036c6dbbcf366fb4655541eacf6
Lucas Cherkewski <lucas@ecustom.ca>
2016-03-11 17:38:40 -0500

    remove orient-related stuff

--- app/contracts/Migration.php
+++ app/contracts/Migration.php
@@ -3,9 +3,7 @@
 namespace Lchski\Contracts;
 
 
-use PhpOrient\PhpOrient;
-
 interface Migration
 {
-	public function __construct( PhpOrient $phporient, DbHelper $dbHelper );
+    public function __construct();
 }
--- app/migrations/BaseMigration.php
+++ app/migrations/BaseMigration.php
@@ -2,29 +2,12 @@
 
 namespace Lchski\Migrations;
 
-use Lchski\Contracts\DbHelper;
 use Lchski\Contracts\Migration;
-use PhpOrient\PhpOrient;
 
 abstract class BaseMigration implements Migration
 {
-	/**
-	 * @var PhpOrient
-	 */
-	protected $phporient;
+    public function __construct()
+    {
 
-	/**
-	 * @var DbHelper
-	 */
-	protected $dbHelper;
-
-	/**
-	 * Grab the OrientDB client and set it as a property, accessible by any Migration.
-	 * @param PhpOrient $phporient
-	 */
-	public function __construct( PhpOrient $phporient, DbHelper $dbHelper )
-	{
-		$this->phporient = $phporient;
-		$this->dbHelper  = $dbHelper;
-	}
+    }
 }
