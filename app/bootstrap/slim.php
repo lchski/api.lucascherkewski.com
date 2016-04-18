05716815ccddd0f821115213cb502b669ecff038
Lucas Cherkewski <lucas@ecustom.ca>
2016-04-17 22:10:45 -0400

    Implement Basic Auth
    that was easy

--- app/bootstrap/slim.php
+++ app/bootstrap/slim.php
@@ -17,3 +17,18 @@ $app = new \Slim\App($c);
 
 // Add our global middleware.
 $app->add( new \Lchski\TrailingSlashMiddleware );
+
+$app->add(new \Slim\Middleware\HttpBasicAuthentication([
+    'users' => [
+        env('API_USERNAME') => env('API_PASSWORD'),
+    ],
+    'relaxed' => ['localhost', 'lucascherkewski.dev', 'api.lucascherkewski.dev'],
+    'rules' => [
+        new \Slim\Middleware\HttpBasicAuthentication\RequestPathRule([
+            'path' => '/',
+        ]),
+        new \Slim\Middleware\HttpBasicAuthentication\RequestMethodRule([
+            'passthrough' => ['GET', 'OPTIONS'],
+        ])
+    ]
+]));
