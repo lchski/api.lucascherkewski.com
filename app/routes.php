cb820d73c84f5aff8ecf2d3a1dd27169cda7d5f2
Lucas Cherkewski <lucas@ecustom.ca>
2016-04-18 20:53:25 -0400

    Nest items/links in route groups

--- app/routes.php
+++ app/routes.php
@@ -9,21 +9,25 @@ $app->group('/migrations', function() {
 
 // Version our routes.
 $app->group('/v1', function() {
-    $this->get('/items', '\\Lchski\\ItemController')->setName('index');
+    $this->group('/items', function() {
+        $this->get('', '\\Lchski\\ItemController')->setName('index');
 
-    $this->post('/items', '\\Lchski\\ItemController')->setName('createSingle');
+        $this->post('', '\\Lchski\\ItemController')->setName('createSingle');
 
-    $this->get('/items/{id:[0-9]+}', '\\Lchski\\ItemController')->setName('getSingle');
+        $this->get('/{id:[0-9]+}', '\\Lchski\\ItemController')->setName('getSingle');
 
-    $this->get('/items/{id:[0-9]+}/links', '\\Lchski\\ItemController')->setName('getSingleLinks');
+        $this->get('/{id:[0-9]+}/links', '\\Lchski\\ItemController')->setName('getSingleLinks');
 
-    $this->get('/items/{id:[0-9]+}/items', '\\Lchski\\ItemController')->setName('getSingleItems');
+        $this->get('/{id:[0-9]+}/items', '\\Lchski\\ItemController')->setName('getSingleItems');
+    });
 
-    $this->get('/links', '\\Lchski\\LinkController')->setName('index');
+    $this->group('/links', function() {
+        $this->get('', '\\Lchski\\LinkController')->setName('index');
 
-    $this->post('/links', '\\Lchski\\LinkController')->setName('createSingle');
+        $this->post('', '\\Lchski\\LinkController')->setName('createSingle');
 
-    $this->get('/links/{id:[0-9]+}', '\\Lchski\\LinkController')->setName('getSingle');
+        $this->get('/{id:[0-9]+}', '\\Lchski\\LinkController')->setName('getSingle');
 
-    $this->get('/links/{id:[0-9]+}/items', '\\Lchski\\LinkController')->setName('getSingleItems');
+        $this->get('/{id:[0-9]+}/items', '\\Lchski\\LinkController')->setName('getSingleItems');
+    });
 });
